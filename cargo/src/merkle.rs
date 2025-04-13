473b71129f6759d5917ffaa1347367b44f751781
kevin xie <xieyijun@xinngkonggroup.com>
2025-04-13 11:44:04 +0800

    feat: merkle tree implementation

--- cargo/src/merkle.rs
+++ cargo/src/merkle.rs
@@ -1,9 +1,24 @@
 
 
-pub struct Merkle {
-    blocks: []
-}
+pub type Block = Vec<u8>;
+pub type Node = Vec<u8>;
 
-struct Block {
-
+/// MerkleTree implements a binary complete Merkle tree data structure such
+/// that every node is cryptographically hashed and is composed of the
+/// hashes of its children. If a node has no child, it is the cryptographic
+/// hash of a data block. A Merkle tree allows for efficient and secure
+/// verification of the existence of data blocks that lead up to a secure
+/// root hash. A data block is any arbitrary data structure that can be
+/// interpreted as a byte slice such as chunks of a file.
+///
+/// Data blocks can be inserted into the Merkle tree in a given order where
+/// the order is critical as it correlates to the construction of the root
+/// hash. When the Merkle tree is ready to be constructed, it is "finalized"
+/// such that the root hash is computed and proofs may be granted along with
+/// verification of said proofs.
+pub struct MerkleTree {
+    blocks: Vec<Block>,
+    nodes: Vec<Node>,
+    root: Option<Node>,
+    dirty: bool,
 }
